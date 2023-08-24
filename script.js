7daf9b3f4b7355fc05bc397890876c4e76135117
Reshik Roy <reshikroy223@gmail.com>
2023-08-25 00:26:34 +0600

    new

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("click", function () {
+    menuIcon.classList.toggle("active");
+});
+
+ScrollReveal({
+    distance: "900px", // Remove the semicolon and use a comma instead
+    duration: 1000,
+    delay: 200,
+    reset: true,
+});
+
+ScrollReveal().reveal('.hero_info', { origin: "bottom" }); // Correct the spelling of 'origin'
+ScrollReveal().reveal('.img_hero', { origin: "right" }); // Correct the spelling of 'origin'
